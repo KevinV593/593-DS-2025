f6a2c32d71270fc727e157480bcf304cc10cd086
Kevin Vaca <kavaca6@espe.edu.ec>
2025-10-29 12:57:18 -0500

    clase

---
+++ I/I.09.Prueba/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {   
+    /*
+    trazaSec(int*** matriz, int& p, int& f, int& c) {
+       acum = c - 1; 
+        for (int i = 0, i <= p, i++) {
+            for (int j = c, j <= 0, j--) {
+                traza += *(*(matriz + i) + j) + (acum)
+            }
+        }
+    }
+    */
+}
+//Reservar, Insertar, Mover (Apuntar)
+
+//I want a proyect in c++ based in OOP, 
+//also make two functions that make a insertion of one element 
+//to a simple list that has like a tributes a pointer and data int. 
+//The functions need to insert elements by the head and the tail
+
+//DEBUG: Parar (breakPoint), Revisar los valores, Ejecutar sentencia 
